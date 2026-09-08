2eb7278fd651b0f9826b8581c037d5c9bdb167c6
SanTiwari07 <sanskartiwari.smt@gmail.com>
2026-09-08 17:02:17 +0530

    chore: Remove scratch and generated files from version control

--- patch_docker_compose.py
+++
@@ -1,8 +0,0 @@
-with open('docker-compose.yml', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace("    mem_limit: 4g\n", "")
-
-with open('docker-compose.yml', 'w', encoding='utf-8') as f:
-    f.write(content)
-print("Patched docker-compose.yml")

2b4292f9bd66f4c7faea18fdf99d20233aab97ff
Shambhavi Patil <shambhavipatil5631@gmail.com>
2026-09-25 00:33:43 +0530

    chore: remove AI scratch files from root directory

--- read_prompt.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('first_prompt.json', 'r', encoding='utf-16') as f:
-    line = f.read().split('.jsonl:', 1)[1] # remove filename prefix added by Select-String
-    data = json.loads(line.strip())
-    print(data['content'])
